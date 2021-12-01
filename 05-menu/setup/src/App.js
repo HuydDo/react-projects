0ef3e41bcb37824a3343128dd1f102a44063f098
HuydDo <huydinhdo@yahoo.com>
2021-12-01 00:07:23 -0700

    modifiedy app

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -9,7 +9,11 @@ function App() {
   return(
     <main>
       <section className="menu section">
-
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
       </section>
     </main>
   )
