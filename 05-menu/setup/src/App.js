7c214a03038d0f7cf5f4a20f18cab47214195ef3
HuydDo <huydinhdo@yahoo.com>
2021-12-02 00:39:14 -0700

    add manual button

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  const [menuItems, setMenuItem] = useState(items);
-  const [categories,setCategories]= useState([]);
-  return(
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    const newItem = items.filter((item) => item.category === category);
+    setMenuItems(newItem);
+  };
+  return (
     <main>
       <section className="menu section">
-        <div className='title'>
+        <div className="title">
           <h2>our menu</h2>
-          <div className='underline'></div>
+          <div className="underline"></div>
         </div>
-        <Categories />
-        <Menu items = {menuItems} />
+        <Categories filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
