e46b4fef50713d29721a13013b311d82045d7bb1
HuydDo <huydinhdo@yahoo.com>
2021-12-02 01:25:49 -0700

    added dynamic buttons

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
-import Menu from "./Menu";
-import Categories from "./Categories";
-import items from "./data";
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
-    const newItem = items.filter((item) => item.category === category);
-    setMenuItems(newItem);
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
   };
+
   return (
     <main>
       <section className="menu section">
@@ -18,7 +24,7 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
