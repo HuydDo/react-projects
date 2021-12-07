5599893c4e10e9ad985f949889f8f2e8e25ecbcb
HuydDo <huydinhdo@yahoo.com>
2021-12-06 23:59:26 -0700

    modified app.js

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,7 +3,8 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
+  const [people,setPeople] = useState(data);
   return <h2>slider project setup</h2>;
-}
 
+}
 export default App;
