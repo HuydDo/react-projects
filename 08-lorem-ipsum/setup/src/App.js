a3fc98f1e560493e2d10e2a0ec080361056b8823
HuydDo <huydinhdo@yahoo.com>
2021-12-15 00:05:13 -0700

    added generate button

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -4,18 +4,23 @@ function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello world')
-  }
+    console.log("hello world");
+  };
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
       <form className="lorem-form" onSubmit={handleSubmit}>
-        <label htmlFor="amount">
-          paragraphs:
-        </label>
+        <label htmlFor="amount">paragraphs:</label>
+        <input type="number" name="amount" id="amount" value={count} 
+        onChange={(e)=>setCount(e.target)}
+        />
+        <button type="submit" className="btn">generate</button>
       </form>
+    <article>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae?</p>
+    </article>
     </section>
   );
 }
