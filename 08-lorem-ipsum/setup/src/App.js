ced8a59292022fdedf43e11752f32ac6bc5d5629
HuydDo <huydinhdo@yahoo.com>
2021-12-14 22:45:27 -0700

    added useState and form

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log('hello world')
+  }
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+      </form>
+    </section>
+  );
 }
 
 export default App;
