407a7a3725f9bd56a119f76b91888d6964628a69
HuydDo <huydinhdo@yahoo.com>
2021-12-15 22:53:56 -0700

    odified handSubmit function with edge cases

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -6,21 +6,36 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
       <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">paragraphs:</label>
-        <input type="number" name="amount" id="amount" value={count} 
-        onChange={(e)=>setCount(e.target)}
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
         />
-        <button type="submit" className="btn">generate</button>
+        <button type="submit" className="btn">
+          generate
+        </button>
       </form>
-    <article>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, quae?</p>
-    </article>
+      <article>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
     </section>
   );
 }
