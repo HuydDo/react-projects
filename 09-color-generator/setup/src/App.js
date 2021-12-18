cf374ca58309a713f1469dffbffa9899e09b2d87
HuydDo <huydinhdo@yahoo.com>
2021-12-17 21:27:36 -0700

    setup App

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [erro, setError] = useState(false);
+  const [list,setList] = useState([]);
+
+  constt handleSubmit = (e) =>{
+    e.prentDefault();
+    console.log('hello');
+  }
+  return  <section className="container">
+    
+  </section>;
 }
 
-export default App
+export default App;
