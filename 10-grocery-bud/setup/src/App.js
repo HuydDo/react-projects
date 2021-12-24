cce2edaa43dfec8b8d1667ff445d40e3735f20c0
HuydDo <huydinhdo@yahoo.com>
2021-12-24 00:47:43 -0700

    added alert msg

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setEditing] = useState(false);
   const [editId, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const [alert, setAlert] = useState({ show: true, msg: "hello", type: "success" });
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
@@ -24,7 +24,7 @@ function App() {
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert}/>}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
