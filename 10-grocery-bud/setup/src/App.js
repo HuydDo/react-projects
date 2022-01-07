31c3701f80501022f8c379c4fd37d6f84bb7a87a
HuydDo <huydinhdo@yahoo.com>
2022-01-07 02:57:09 -0700

    modified Alert and clearList, removeItem

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -12,24 +12,34 @@ function App() {
     e.preventDefault();
     if (!name) {
       //display alert
-     showAlert(true,'danger','please enter value')
+      showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
       // editing
     } else {
       //show alert
+      showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
     }
   };
 
-  const showAlert = (show=false,type="",msg="") => {
-    setAlert({show,type,msg})
-  }
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  };
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
+  };
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
@@ -46,8 +56,10 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <List items={list} removeItem={removeItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
