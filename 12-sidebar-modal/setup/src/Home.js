4875e3adc9063ea84a045947c0a27ad1a9751cb0
HuydDo <huydinhdo@yahoo.com>
2022-01-14 23:42:10 -0700

    add close modal and home

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,16 +1,20 @@
 import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
-import { AppContext, useGlobalContext } from "./context";
+// import { AppContext, useGlobalContext } from "./context";
+import {  useGlobalContext } from "./context";
+
 const Home = () => {
   // const data = useContext(AppContext);
-  const data = useGlobalContext(AppContext); //use custom hook
-  console.log(data);
+  const {openSideBar, openModal} = useGlobalContext(); //use custom hook
+  // console.log(data);
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSideBar}>
         <FaBars />
       </button>
-      <button className="btn">show modal</button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
     </main>
   );
 };
