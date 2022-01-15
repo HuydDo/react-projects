8528ee1a8a609f980383c77a83fa451f3a11591b
HuydDo <huydinhdo@yahoo.com>
2022-01-14 23:14:29 -0700

    create custom hook instead of useContext

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,7 +1,10 @@
 import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
-
+import { AppContext, useGlobalContext } from "./context";
 const Home = () => {
+  // const data = useContext(AppContext);
+  const data = useGlobalContext(AppContext); //use custom hook
+  console.log(data);
   return (
     <main>
       <button className="sidebar-toggle">
