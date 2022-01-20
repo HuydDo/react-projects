f8dcf6088ec360fdc514a785c61e1412c34d2d0c
HuydDo <huydinhdo@yahoo.com>
2022-01-19 21:04:01 -0700

    modified Hero, Navbar, Sidebar, and context components

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,19 +1,22 @@
-import React, { useState, useContext } from "react";
-import sublinks from "./data";
-
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
-
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
   const openSidebar = () => {
     setIsSidebarOpen(true);
   };
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
-  const openSubmenu = () => {
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
   const closeSubmenu = () => {
@@ -23,19 +26,21 @@ const AppProvider = ({ children }) => {
   return (
     <AppContext.Provider
       value={{
-        isSubmenuOpen,
         isSidebarOpen,
-        openSubmenu,
         openSidebar,
-        closeSubmenu,
         closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
       }}
     >
       {children}
     </AppContext.Provider>
   );
 };
-
+// make sure use
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
