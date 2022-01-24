bf14965cb118fbe51166497f180913d5b45bf066
HuydDo <huydinhdo@yahoo.com>
2022-01-24 00:00:02 -0700

    modified Submenu component

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,7 +1,19 @@
-import React, { useState, useRef, useEffect } from 'react'
-
+import React, { useState, useRef, useEffect } from "react";
+import { useGlobalContext } from "./context";
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const { isSubmenuOpen, location } = useGlobalContext();
+  const container = useRef(null);
+  useEffect(() => {
+    
+  }, [location]);
+  return (
+    <aside
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
+      submenu
+    </aside>
+  );
+};
 
-export default Submenu
+export default Submenu;
