b3c182c5736597586aba9353e3451cbcd9958cc0
HuydDo <huydinhdo@yahoo.com>
2022-01-29 23:34:04 -0700

    added remove,increse, decrease functions

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -16,15 +16,24 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const clearCart = () => {
     dispatch({ type: "CLEAR_CART" });
-  };
+  }
   const remove = (id) => {
     dispatch({ type: "REMOVE", payload: id });
   };
+  const increase = (id) =>{
+    dispatch({type:'INCREASE', payload: id})
+  }
+  const decrease = (id) =>{
+    dispatch({type:'DECREASE', payload: id})
+  }
   return (
     <AppContext.Provider
       value={{
         ...state,
         clearCart,
+        remove,
+        increase,
+        decrease,
       }}
     >
       {children}
