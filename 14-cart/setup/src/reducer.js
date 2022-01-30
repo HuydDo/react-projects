1ba6b522b76047ee535f6a04f1735bb3ad24ac4c
HuydDo <huydinhdo@yahoo.com>
2022-01-29 17:32:05 -0700

    added remove item

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,15 +1,25 @@
-const reducer = (state, action) =>{
-  // if(action.type==='CLEAR_CART'){
-  //   return {...state,cart:[]}
-  // }
-  // return state
-  switch(action.type){
-    case 'CLEAR_CART':
-      return {...state,cart:[]}
-    default:
-      return state
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
   }
-  
-}
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
+  return state;
+  // switch(action.type){
+  //   case 'CLEAR_CART':
+  //     return {...state,cart:[]}
+      //  case 'REMOVE':
+      //   return {
+      //     ...state,
+      //     cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+      //   };
+  //   default:
+  //     return state
+  // }
+};  
 
-export default reducer
+export default reducer;
