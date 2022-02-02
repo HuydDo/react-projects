cc101abf117f253438c5afbbe6574f79647df846
HuydDo <huydinhdo@yahoo.com>
2022-02-01 23:08:22 -0700

    modified about page

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quo architecto voluptatibus. Ducimus, aperiam. Rerum totam quasi officiis, blanditiis facere, soluta qui ducimus maxime neque aliquid quas, accusamus odio magni.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
