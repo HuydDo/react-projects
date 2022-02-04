08ca808ecbcbaf7e7cdbec0b1c2998f23c2facce
HuydDo <huydinhdo@yahoo.com>
2022-02-04 00:18:43 -0700

    added handleSubmit to prevent open new page

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -4,12 +4,21 @@ import { useGlobalContext } from "../context";
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
   const searchValue = React.useRef("");
-  const searchCoctail =()=>{
-    setSearchTerm(searchValue.current.value)
-  }
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCoctail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <section className="section search">
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">search your favorite cocktail</label>
           <input
