59a8de1048c6255249b4e38147819f65f7ad39e6
HuydDo <huydinhdo@yahoo.com>
2022-02-04 00:08:15 -0700

    added search form

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React from "react";
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  const {setSearchTerm} = useGlobalContext()
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+  const searchCoctail =()=>{
+    setSearchTerm(searchValue.current.value)
+  }
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
-  )
-}
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCoctail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
