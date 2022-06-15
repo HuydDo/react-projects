a0198aa6fa9dab2a1f11fa887c8790ec9accf231
HuydDo <huydinhdo@yahoo.com>
2022-06-15 00:30:23 -0600

    review, accordion, menu projects updated

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
 import data from "./data";
 import SingleQuestion from "./Question";
-function App() {
+
+function App(){
+  const [questions,setQuestions]= useState(data);
+    return <main className="container">
+      <h3>questions and anwers about login</h3>
+      <section className="info">
+        {
+          questions.map((question)=>{
+            return <SingleQuestion key={question.id} {...question}/>
+          })
+        }
+      </section>
+    </main>
+}
+export default App;
+/* function App() {
   const [questions, setQuestions] = useState(data);
   return (
     <main>
@@ -18,4 +33,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -6,6 +6,34 @@ const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCatogories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu secttion">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories = {categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+export default App;
+
+/* function App() {
+  const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
@@ -32,3 +60,4 @@ function App() {
 }
 
 export default App;
+ */
