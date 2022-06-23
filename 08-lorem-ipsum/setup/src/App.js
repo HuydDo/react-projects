95f73b990bbb2266dba1ee6481a08863d676eb9a
HuydDo <huydinhdo@yahoo.com>
2022-06-23 01:23:43 -0600

    refactor lorem-ipsum proj

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import data from "./data";
+
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
@@ -7,17 +8,18 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     let amount = parseInt(count);
-    if (count <= 0) {
+    if(count<=0){
       amount = 1;
     }
-    if (count > 8) {
-      amount = 8;
+    if(count > data.length-1){
+      amount = data.length - 1;
     }
-    setText(data.slice(0, amount));
+    setText(data.slice(0,amount))
   };
+
   return (
     <section className="section-center">
-      <h3>tired of boring lorem ipsum?</h3>
+      <h3>tired of boring lorem ipsum</h3>
       <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">paragraphs:</label>
         <input
@@ -27,17 +29,55 @@ function App() {
           value={count}
           onChange={(e) => setCount(e.target.value)}
         />
-        <button type="submit" className="btn">
-          generate
-        </button>
+        <button type="submit" className="btn">generate</button>
       </form>
-      <article>
-        {text.map((item, index) => {
-          return <p key={index}>{item}</p>;
-        })}
+      <article className="lorem-text">
+       {text.map((item,index)=>{
+        return <p key={index}>{item}</p>
+       })}
       </article>
     </section>
   );
 }
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [text, setText] = useState([]);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     let amount = parseInt(count);
+//     if (count <= 0) {
+//       amount = 1;
+//     }
+//     if (count > 8) {
+//       amount = 8;
+//     }
+//     setText(data.slice(0, amount));
+//   };
+//   return (
+//     <section className="section-center">
+//       <h3>tired of boring lorem ipsum?</h3>
+//       <form className="lorem-form" onSubmit={handleSubmit}>
+//         <label htmlFor="amount">paragraphs:</label>
+//         <input
+//           type="number"
+//           name="amount"
+//           id="amount"
+//           value={count}
+//           onChange={(e) => setCount(e.target.value)}
+//         />
+//         <button type="submit" className="btn">
+//           generate
+//         </button>
+//       </form>
+//       <article>
+//         {text.map((item, index) => {
+//           return <p key={index}>{item}</p>;
+//         })}
+//       </article>
+//     </section>
+//   );
+// }
 
 export default App;
