e983d89ac555a83da9bb1dfd29bd3d901827a470
HuydDo <huydinhdo@yahoo.com>
2022-06-25 16:23:26 -0600

    refactor grocery-bud proj

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -2,31 +2,31 @@ import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
 
-const getLocalStorage = ()=> {
-  let list = localStorage.getItem('list');
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
   if(list){
-    return JSON.parse(localStorage.getItem('list'))
+    return JSON.parse(list)
+    // return JSON.parse(localStorage.getItem('list'))
   }
   else{
     return []
   }
-}
+};
 function App() {
   const [name, setName] = useState("");
   const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
-  const [editId, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlerat] = useState({ show: false, msg: "", type: "" });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
-      //display alert
       showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
-      // editing
       setList(
         list.map((item) => {
-          if (item.id === editId) {
+          if (item.id === editID) {
             return { ...item, title: name };
           }
           return item;
@@ -34,10 +34,9 @@ function App() {
       );
       setName("");
       setEditID(null);
-      setIsEditing(false);
-      showAlert(true,'success','value changed');
+      setEditID(false);
+      showAlert(true, "success", "value changed");
     } else {
-      //show alert
       showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
@@ -46,26 +45,30 @@ function App() {
   };
 
   const showAlert = (show = false, type = "", msg = "") => {
-    setAlert({ show, type, msg });
+    setAlerat({ show, type, msg });
   };
 
   const clearList = () => {
     showAlert(true, "danger", "empty list");
     setList([]);
   };
+
   const removeItem = (id) => {
     showAlert(true, "danger", "item removed");
     setList(list.filter((item) => item.id !== id));
   };
+
   const editItem = (id) => {
     const specificItem = list.find((item) => item.id === id);
     setIsEditing(true);
     setEditID(id);
     setName(specificItem.title);
   };
-  useEffect(()=>{
-    localStorage.setItem('list', JSON.stringify(list))
-  },[list])
+
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
@@ -75,7 +78,7 @@ function App() {
           <input
             type="text"
             className="grocery"
-            placeholder="e.g. eggs"
+            placeholder="e.g eggs"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
@@ -95,5 +98,99 @@ function App() {
     </section>
   );
 }
+
+// const getLocalStorage = ()=> {
+//   let list = localStorage.getItem('list');
+//   if(list){
+//     return JSON.parse(localStorage.getItem('list'))
+//   }
+//   else{
+//     return []
+//   }
+// }
+// function App() {
+//   const [name, setName] = useState("");
+//   const [list, setList] = useState(getLocalStorage());
+//   const [isEditing, setIsEditing] = useState(false);
+//   const [editId, setEditID] = useState(null);
+//   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (!name) {
+//       //display alert
+//       showAlert(true, "danger", "please enter value");
+//     } else if (name && isEditing) {
+//       // editing
+//       setList(
+//         list.map((item) => {
+//           if (item.id === editId) {
+//             return { ...item, title: name };
+//           }
+//           return item;
+//         })
+//       );
+//       setName("");
+//       setEditID(null);
+//       setIsEditing(false);
+//       showAlert(true,'success','value changed');
+//     } else {
+//       //show alert
+//       showAlert(true, "success", "item added to the list");
+//       const newItem = { id: new Date().getTime().toString(), title: name };
+//       setList([...list, newItem]);
+//       setName("");
+//     }
+//   };
+
+//   const showAlert = (show = false, type = "", msg = "") => {
+//     setAlert({ show, type, msg });
+//   };
+
+//   const clearList = () => {
+//     showAlert(true, "danger", "empty list");
+//     setList([]);
+//   };
+//   const removeItem = (id) => {
+//     showAlert(true, "danger", "item removed");
+//     setList(list.filter((item) => item.id !== id));
+//   };
+//   const editItem = (id) => {
+//     const specificItem = list.find((item) => item.id === id);
+//     setIsEditing(true);
+//     setEditID(id);
+//     setName(specificItem.title);
+//   };
+//   useEffect(()=>{
+//     localStorage.setItem('list', JSON.stringify(list))
+//   },[list])
+//   return (
+//     <section className="section-center">
+//       <form className="grocery-form" onSubmit={handleSubmit}>
+//         {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+//         <h3>grocery bud</h3>
+//         <div className="form-control">
+//           <input
+//             type="text"
+//             className="grocery"
+//             placeholder="e.g. eggs"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//           />
+//           <button type="submit" className="submit-btn">
+//             {isEditing ? "edit" : "submit"}
+//           </button>
+//         </div>
+//       </form>
+//       {list.length > 0 && (
+//         <div className="grocery-container">
+//           <List items={list} removeItem={removeItem} editItem={editItem} />
+//           <button className="clear-btn" onClick={clearList}>
+//             clear items
+//           </button>
+//         </div>
+//       )}
+//     </section>
+//   );
+// }
 
 export default App;
