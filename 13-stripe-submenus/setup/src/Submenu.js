285de18479b8b8f0d53d52b7f5759a633fb6e13b
HuydDo <huydinhdo@yahoo.com>
2022-08-22 22:30:19 -0600

    refactore stripe project

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,14 +1,26 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useGlobalContext } from "./context";
 
-const Submenu = () =>{
-  const {isSubmenuOpen} = useGlobalContext
+const Submenu = () => {
+  const { isSubmenuOpen, location } = useGlobalContext();
+  const container = useRef(null);
+  useEffect(() => {
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`
+    submenu.style.top =`${bottom}px`
+  }, [location]);
+  return (
+    <aside
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
+      submenu
+    </aside>
+  );
+};
 
-  return <aside className={`${isSubmenuOpen ? 'submenu show':
- 'submenu'}`}>submenu</aside>
-}
-
-export default Submenu
+export default Submenu;
 
 // const Submenu = () => {
 //   const {
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -6,7 +6,7 @@ const AppContext = React.createContext();
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
-
+  const [location, setLocation] = useState({});
   const openSidebar = () => {
     setIsSidebarOpen(true);
   };
@@ -15,7 +15,8 @@ export const AppProvider = ({ children }) => {
     setIsSidebarOpen(false);
   };
 
-  const openSubmenu = () => {
+  const openSubmenu = (text, coordinates) => {
+    setLocation(coordinates);
     setIsSubmenuOpen(true);
   };
 
@@ -25,14 +26,15 @@ export const AppProvider = ({ children }) => {
 
   return (
     <AppContext.Provider
-      value={
-        {isSubmenuOpen,
+      value={{
+        isSubmenuOpen,
         isSidebarOpen,
         openSubmenu,
         openSidebar,
         closeSubmenu,
-        closeSidebar}
-      }
+        closeSidebar,
+        location,
+      }}
     >
       {children}
     </AppContext.Provider>
