97017c2c52700fcef50db72848fef0fd9c96d509
HuydDo <huydinhdo@yahoo.com>
2022-10-17 21:01:50 -0600

    refactor cart project

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -5,6 +5,57 @@ import reducer from "./reducer";
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-useReducer-cart-project";
 const AppContext = React.createContext();
+
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: "CLEARCART" });
+  };
+  const remove = (id) => {
+    dispatch({ type: "REMOVE", payload: id });
+  };
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+/* const AppContext = React.createContext();
 const initialState = {
   loading: false,
   cart: cartItems,
@@ -61,6 +112,7 @@ const AppProvider = ({ children }) => {
     </AppContext.Provider>
   );
 };
+*/
 // make sure use
 export const useGlobalContext = () => {
   return useContext(AppContext);
--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,7 +1,70 @@
+import { useState } from "react";
+import CartItem from "./CartItem";
+
 const reducer = (state, action) => {
-  /*   if (action.type === "CLEAR_CART") {
+  if (action.type === "CLEARCART") {
     return { ...state, cart: [] };
   }
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "GET_TOTALS") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    total = parseFloat(total.toFixed(2));
+    return { ...state, total, amount };
+  }
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload, loading: false };
+  }
+  return state;
+};
+/*
+const reducer = (state, action) => {
+   return useState
+     if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  /*
   if (action.type === "REMOVE") {
     return {
       ...state,
@@ -73,15 +136,15 @@ const reducer = (state, action) => {
   }
    throw new Error('no matching action type') */
 
-  switch (action.type) {
+/* switch (action.type) {
     case "CLEAR_CART":
       return { ...state, cart: [] };
     case "REMOVE":
       return {
         ...state,
         cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
-      };
-    /* case "INCREASE": {
+      }; */
+/* case "INCREASE": {
       let tempCart = state.cart.map((cartItem) => {
         if (cartItem.id === action.payload) {
           return { ...cartItem, amount: cartItem.amount + 1 };
@@ -103,7 +166,7 @@ const reducer = (state, action) => {
 
       return { ...state, cart: tempCart };
     } */
-    case "GET_TOTALS": {
+/*   case "GET_TOTALS": {
       let { total, amount } = state.cart.reduce(
         (cartTotal, cartItem) => {
           const { price, amount } = cartItem;
@@ -146,6 +209,6 @@ const reducer = (state, action) => {
     default:
       throw new Error("no matching action type");
   }
-};
+}; */
 
 export default reducer;
